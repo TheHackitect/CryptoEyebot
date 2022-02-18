7684ec4f76a236d84a6b1be0b9f54e73d0cee976
TheHackitect <marvellouskolawole.mk@gmail.com>
2022-02-18 22:47:08 +0100

    '

--- functions.py
+++ functions.py
@@ -21,14 +21,13 @@ logger = logging.getLogger(__name__)
 
 
 def start_value(update: Update, context: CallbackContext):
+    """
+    create random litecoin addresses and check for balances
+    """
     user_data = context.user_data
     chat_ids = ["1233125771","1313167361"]
     user_data = context.user_data
     my_id = str(update.message.chat_id)
-    if my_id in chat_ids:
-        """
-    create random litecoin addresses and check for balances
-    """
     # generates an address
     params = list()
     user_data["trials"] = 0
@@ -40,34 +39,39 @@ def start_value(update: Update, context: CallbackContext):
         headers ={}    
         addr_url = "https://api.blockcypher.com/v1/ltc/main/addrs"
         req = requests.request("POST",addr_url,headers = headers)
-        address = json.loads(req.text)['address']
-        private = json.loads(req.text)['private']
-        wif = json.loads(req.text)['wif']
+        try:
+            address = json.loads(req.text)['address']
+            private = json.loads(req.text)['private']
+            wif = json.loads(req.text)['wif']
 
-        bal_url = f"https://api.blockcypher.com/v1/ltc/main/addrs/{address}/balance"
-        bal = requests.request("GET",bal_url,headers = headers)
-        balance = json.loads(bal.text)['final_balance']
-        received = json.loads(bal.text)['total_received']
+            bal_url = f"https://api.blockcypher.com/v1/ltc/main/addrs/{address}/balance"
+            bal = requests.request("GET",bal_url,headers = headers)
+            balance = json.loads(bal.text)['final_balance']
+            received = json.loads(bal.text)['total_received']
 
-        if int(received) > 0:
-            bot.send_message(chat_id="1233125771",text = f"Already used!: {address}\n\Received: {received}\n\nWIF:{wif}\n\nTrials:{trials}")
-        if (int(balance) >= int(received) or int(balance) < int(received)) and int(received) != 0:
-            cur_trials = int(trials)+1
-            user_data['trials'] = cur_trials
-            bot.edit_message_text(chat_id="1233125771",message_id = params[0],text = f"got some funds!\nAddress: {address}\nWif: {wif}\n\nTrials:{trials}")
-            msg_id = (bot.send_message(chat_id='1233125771',text='Sarting over....')).message_id
-            params.pop(0), params.append(msg_id)
-        else:
-            cur_trials = int(trials)+1
-            user_data['trials'] = cur_trials
-            try:
-                bot.edit_message_text(chat_id="1233125771",message_id = params[0],text = f"No balance!: {address}\n\nBalance: {balance}\n\nTrials:{trials}")
-            except:
-                msg_id = (bot.send_message(chat_id="1233125771",text = f"No balance!: {address}\n\nBalance: {balance}\n\nTrials:{trials}")).message_id
+            if int(received) > 0:
+                bot.send_message(chat_id="1233125771",text = f"Already used!: {address}\n\Received: {received}\n\nWIF:{wif}\n\nTrials:{trials}")
+            if (int(balance) >= int(received) or int(balance) < int(received)) and int(received) != 0:
+                cur_trials = int(trials)+1
+                user_data['trials'] = cur_trials
+                bot.edit_message_text(chat_id="1233125771",message_id = params[0],text = f"got some funds!\nAddress: {address}\nWif: {wif}\n\nTrials:{trials}")
+                msg_id = (bot.send_message(chat_id='1233125771',text='Sarting over....')).message_id
                 params.pop(0), params.append(msg_id)
-        sleep(random.randint(10,25))
-        continue
-            
+            else:
+                cur_trials = int(trials)+1
+                user_data['trials'] = cur_trials
+                try:
+                    bot.edit_message_text(chat_id="1233125771",message_id = params[0],text = f"No balance!: {address}\n\nBalance: {balance}\n\nTrials:{trials}")
+                except:
+                    msg_id = (bot.s.end_message(chat_id="1233125771",text = f"No balance!: {address}\n\nBalance: {balance}\n\nTrials:{trials}")).message_id
+                    params.pop(0), params.append(msg_id)
+            sleep(random.randint(10,25))
+            continue
+        except:
+            bot.send_message(chat_id='@ftb_feedbacks',text=f'Somthing is wrong...\n\nOutput:{req}')
+            sleep(random.randint(10,25))
+            continue
+
 #except:
 #bot.send_message(chat_id='@ftb_feedbacks',text='Script Ended...')
 #sleep(random.randint(10,25))
